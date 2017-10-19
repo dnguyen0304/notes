7791e7df7064fd8800f57303277d725ed9a1c094
Duy Nguyen <dnguyen0304@gmail.com>
2017-10-19 13:51:00 -0400

    Rewrote tree-search-breadth-first for Go.

--- go/src/github.com/dnguyen0304/tree-search-breadth-first/tree-search-breadth-first.go
+++ go/src/github.com/dnguyen0304/tree-search-breadth-first/tree-search-breadth-first.go
@@ -1,3 +1,16 @@
+// Package main implements a breadth-first-search algorithm for tree data
+// structures.
+//
+// What type of tree is this? Would it be acceptable to assume the tree is a
+// binary tree? How is the tree represented in memory? Would it be acceptable
+// to assume the tree is akin to a linked list where the algorithm is passed
+// the head node (or the root node in this case) and traverses it as needed?
+// Am I allowed to use other data structures? Am I allowed to implement other
+// data structures? Would you prefer an iterative or recursive approach? Does
+// the entire tree fit in memory? What should be done with the node upon
+// visiting it? Would it be acceptable merely to print its data?
+//
+// How do you declare generic containers?
 package main
 
 import (
@@ -7,24 +20,32 @@ import (
 
 var ErrEmptyQueue = errors.New("The queue is empty.")
 
-// ArrayQueue is a queue data structure backed by an array (i.e. slice). Go
-// version 1.0 removes the container package from the standard library and
-// subsequently the vector data structure. The recommendation moving forward
-// is to use channels or slices.
+// ArrayQueue is a queue backed by an array (i.e. slice).
+//
+// Go version 1.0 removes the container package from the standard library and
+// subsequently the vector. The recommendation moving forward is to use
+// channels or slices.
+//
+// The vector in Go should not be confused with that in Java. The Vector in
+// Java is a dynamically-sized array. Unlike the ArrayList, it is synchronized
+// to some degree, resizes by 100% to amortize insertion cost, and
+// conventionally is viewed as deprecated.
 type ArrayQueue struct {
 	array []*Node
 }
 
-// Push adds an element to the tail of the queue. This operation is not
-// thread-safe. The time complexity is O(1) amortized depending on the language
-// implementation.
+// Push adds an element to the tail of the queue.
+//
+// The time complexity is O(1) amortized depending on the language
+// implementation. This operation is not thread-safe.
 func (queue *ArrayQueue) Push(node *Node) {
 	queue.array = append(queue.array, node)
 }
 
-// Pop removes an element from the head of the queue. This method returns
-// ErrEmptyQueue if the queue is empty. This operation is not thread-safe. The
-// time complexity is O(1).
+// Pop removes an element from the head of the queue.
+//
+// This method returns ErrEmptyQueue if the queue is empty. The time
+// complexity is O(1). This operation is not thread-safe.
 func (queue *ArrayQueue) Pop() (*Node, error) {
 	if len(queue.array) == 0 {
 		return nil, ErrEmptyQueue
@@ -34,8 +55,11 @@ func (queue *ArrayQueue) Pop() (*Node, error) {
 	return node, nil
 }
 
-// Node is a string node data structure.
+// Node is a string node for a binary tree.
 type Node struct {
+    // The fields must be defined as pointers to avoid a "invalid recursive
+	// type" error. This works because the compiler can calculate the size of
+    // pointers.
 	data string
 	left *Node
 	right *Node
@@ -68,7 +92,9 @@ func NewTree() *Node {
 	return &a
 }
 
-// SearchTree traverses the tree data structure using breadth-first searching.
+// SearchTree traverses the tree using breadth-first-search starting at the
+// given node.
+//
 // The time complexity is O(n), where n is the number of nodes in the tree.
 func SearchTree(queue *ArrayQueue, node *Node) {
 	// Base Case
@@ -91,8 +117,8 @@ func SearchTree(queue *ArrayQueue, node *Node) {
 }
 
 func main() {
-	// When there is no specified size, declare a zero value slice variable
-	// rather than declaring a slice literal or using the make function.
+	// When the size is not known, declare a zero-valued slice variable rather
+	// than declaring a slice literal or using the make function.
 	var array []*Node
 	queue := &ArrayQueue{array: array}
 
